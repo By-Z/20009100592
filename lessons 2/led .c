4f609b3ccfbd00dfbbc0ca6e61340f9f632dd5ef
By-Z <87235908+By-Z@users.noreply.github.com>
2021-07-17 08:07:25 +0800

    Add files via upload

---
+++ lessons 2/led .c
@@ -0,0 +1,27 @@
+// C++ code
+//
+int i=13;
+int j=0;
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop()
+{
+  for(i=13;i>3;i--)
+  {
+    digitalWrite(i,HIGH);
+    delay(250);
+    digitalWrite(i,LOW);
+}
+}
